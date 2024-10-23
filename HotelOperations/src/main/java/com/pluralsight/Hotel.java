b836b60c7833532a9939474afa5ad47fc11a2d72
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-10-23 10:27:59 -0400

    Fineshed excersise 3

---
+++ HotelOperations/src/main/java/com/pluralsight/Hotel.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+public class Hotel {
+    private String name;
+    private int numberOfSuites, numberOfRooms, bookedSuites, bookedBasicRooms;
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = 0;
+        this.bookedBasicRooms = 0;
+    }
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) throws Exception {
+        if (this.numberOfRooms < getAvailableRooms()) {
+            return false;
+        }
+        if (isSuite) {
+            bookedSuites += numberOfRooms; return true;
+        }
+        bookedBasicRooms += numberOfRooms; return true;
+
+    }
+
+    public int getAvailableSuites() {
+        return numberOfSuites - bookedSuites;
+    }
+
+    public int getAvailableRooms() {
+        return numberOfRooms - bookedBasicRooms;
+    }
+
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public int getBookedBasicRooms() {
+        return bookedBasicRooms;
+    }
+}
